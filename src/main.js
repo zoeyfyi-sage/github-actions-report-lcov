31627be86289f5c148341a77c623417b637e8bcd
Zoey Sheffield <Zoey.Sheffield@sage.com>
2025-02-06 23:00:07 +0000

    use flat

--- src/main.js
+++ src/main.js
@@ -133,7 +133,7 @@ async function genhtml(coverageFile, baselineFile, diffFilePath, tmpPath) {
     args.push('--baseline-file', baselineFile, '--diff-file', diffFilePath);
   }
 
-  args.push('--hierarchical', '-branch-coverage', '--legend', '--dark-mode')
+  args.push('--flat', '-branch-coverage', '--legend', '--dark-mode')
   args.push('--source-directory', 'assets/portal')
   // args.push('--ignore-errors', 'unmapped');
   // args.push('--rc', 'lcov_branch_coverage=1');
